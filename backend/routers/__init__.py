ed2877e7c6a9ba7ddf671dc21ca6fadc7a3ba2e9
garysteele777 <155407816+garysteele777@users.noreply.github.com>
2025-12-30 13:38:08 -0600

    Add catalog API endpoints

--- backend/routers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .dbmodels import Decade, Genre
-from .collection_models import CollectionCategory, Collection
-
-__all__ = [
-    "Decade",
-    "Genre",
-    "CollectionCategory",
-    "Collection",
-]
