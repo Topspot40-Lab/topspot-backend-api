6a94d169102464f7c773083c8a6e8d69d2ef9795
garysteele777 <155407816+garysteele777@users.noreply.github.com>
2026-01-15 06:37:00 -0600

    added netlify.app in middleware in main program

--- backend/main.py
+++ backend/main.py
@@ -50,6 +50,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "https://topspot40.com",
+        "https://topspot40-frontend-app.netlify.app",
+
     ],
     allow_credentials=False,
     allow_methods=["*"],
